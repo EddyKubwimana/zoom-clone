c520e47fc412571399c5ec10de97802105427f21
EddyKubwimana <kubwimanaeddy1@gmail.com>
2022-10-15 13:15:04 +0000

    sending dataframe over socket

--- client.py
+++ client.py
@@ -1,10 +1,11 @@
 import socket
 import numpy as np
 import pandas
+import pickle
 
 while True:
     host = '172.16.2.42'
-    port = 5090
+    port = 5070
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.connect((host, port))
     print("enter the name of student who came in class")
@@ -16,19 +17,17 @@ while True:
         id1 = input('enter the Id of the student')
         name1 = input( "enter the name of the student")
         course1 = input( "enter the name course a student has taken")
-        id.append(id)
+        id.append(id1)
         name.append(name1)
-        course.append(course)
+        course.append(course1)
         print("Don't you want to add another student, if yes, tap y otherwise press any key")
         option = input()
         if option.lower() == 'y':
             b = False
         else:
-            continue
-    pd = pandas
-    pd["id"] = np.array(id)
-    pd["name"] = np.array(name)
-    pd["course"] = np.array(course)
-    server.send(pd.encode('utf-8'))
+            print("continue enter the name")
+    table = pandas.DataFrame({"id": id,"name": name, "course": course})
+    my_bytes = pickle.dumps(table, protocol=4)
+    server.send(my_bytes)
     reply = server.recv(1024).decode('utf-8')
     print(reply)
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import socket
 import threading
+import pickle
 import pandas as pd
 host = '172.16.2.42'
-port = 5090
+port = 5070
 FORMAT = 'utf-8'
 SIZE = 1024
 #function that receive and send the message
@@ -10,7 +11,8 @@ SIZE = 1024
 def clients(communication_socket , address):
     
     print(f"you are connected to {address}")
-    message = communication_socket.recv(SIZE).decode(FORMAT)
+    message = communication_socket.recv(SIZE)
+    message = pickle.loads(message)
     message.to_csv('studentpresent.csv')
     print("here is your message")
     print(message)
